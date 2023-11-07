092ecfb7538aa9dd5b9dcc2c9a6a4583055aca70
OlhaRadchenko <142627165+OlhaRadchenko@users.noreply.github.com>
2023-11-07 20:09:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-from datetime import date, datetime
-
+from datetime import date, timedelta
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    birthdays_per_week = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
 
+    today = date.today()
+
+    start_of_week = today - timedelta(days=today.weekday())
+
+    for user in users:
+        name = user['name']
+        birthday = user['birthday']
+
+        if birthday.replace(year=today.year) < today:
+            birthday = birthday.replace(year=today.year + 1)
+
+        day_of_week = birthday.strftime('%A')
+
+        birthdays_per_week[day_of_week].append(name)
+
+    for i in range(today.weekday()):
+        day = (start_of_week + timedelta(days=i)).strftime('%A')
+        birthdays_per_week[day] += birthdays_per_week[day_of_week]
+        birthdays_per_week[day_of_week] = []
+
+    return birthdays_per_week
 
 if __name__ == "__main__":
-    users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
-    ]
+    users = [{'name': 'Bill', 'birthday': date(1990, 11, 5)},
+             {'name': 'Jan', 'birthday': date(1992, 11, 7)},
+             {'name': 'Kim', 'birthday': date(1995, 11, 9)}]
 
     result = get_birthdays_per_week(users)
-    print(result)
-    # Виводимо результат
+
     for day_name, names in result.items():
         print(f"{day_name}: {', '.join(names)}")
